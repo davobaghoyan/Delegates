61f84a3968c0814d841fde3334c6260bc78d21e6
davobaghoyan <57056544+davobaghoyan@users.noreply.github.com>
2019-12-03 18:04:55 +0400

    Add files via upload

---
+++ Dice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollingDice
+{
+    class Dice
+    {
+        public   delegate void mydel (string x);
+        event mydel DoubleFor, Over20;
+        private int Side, Current, Sum, Index;
+        Random rand = new Random();
+        public void Roll ()
+        {
+             
+            Side = rand.Next(1, 7);
+
+            if (Index < 5)
+             Sum += Side;
+
+            if (Index == 5)
+            {
+                Index = 0;
+                Console.WriteLine("--------------------");
+                if (Sum >= 20)
+                {
+                    Over20("Sum of the last 5 tosses is over 20");
+                }
+                Sum = Side;
+            }
+            Console.WriteLine($"You tossed number {Side}");
+
+
+            if (Side == 4  && Current == 4)
+             DoubleFor("You get 2 fours in the row");
+             Current = Side;
+            Index++;
+        }
+
+        public void RegisterEventDoublefor ( mydel f)
+        {
+            this.DoubleFor += f;
+        }
+        public void RegisterEventOver20(mydel f)
+        {
+            this.Over20 += f;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollingDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice MyDice = new Dice();
+            MyDice.RegisterEventDoublefor(new Dice.mydel(Program.f));
+            MyDice.RegisterEventOver20(new Dice.mydel(Program.f1));
+            for (int i = 0; i < 100; i++ )
+            MyDice.Roll();
+            Console.Read();
+        }
+
+        public static void f (string a)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(a);
+            Console.ResetColor();
+        }
+
+        public static void f1(string a)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(a);
+            Console.ResetColor();
+        }
+    }
+}
